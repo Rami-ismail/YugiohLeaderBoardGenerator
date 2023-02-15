0f7e459d97ecd4c933d426b3234f2096e52fc586
rami ismail <80627830+Rami-ismail@users.noreply.github.com>
2023-02-15 03:54:16 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -4,14 +4,12 @@ import pandas as pd
 
 today = date.today().strftime("%d-%m-%Y")
 
-
 class Player:
     def __init__(self, name, deck, score):
         self.name = name
         self.deck = deck
         self.score = score
-
-
+        
 def readData():
     data = pd.read_excel('./Input/Leaderboard-Input.xlsx')
     leaderboard_data = []
@@ -19,7 +17,6 @@ def readData():
         row = data.iloc[index].to_numpy()
         leaderboard_data.append(Player(row[0], row[1], row[2]))
     return leaderboard_data
-
 
 def splitData():
     leaderboard_data = readData()
@@ -30,57 +27,34 @@ def splitData():
         return smaller_arrays
     else:
         return [leaderboard_data]
-
-
+    
 def exportLeaderboard():
     data = splitData()
     for i in range(len(data)):
         leaderboard = str(today) + " - leaderboard pt"+str(i+1)+".png"
-        DrawLeaderBoard(data[i], leaderboard, i+1)
-
-
-def DrawLeaderBoard(leaderboard_data, leaderboardName, counter):
+        drawLeaderBoard(data[i], leaderboard, i+1)
+        
+def drawLeaderBoard(leaderboard_data, leaderboardName, counter):
     background_image = Image.open("./Assets/Backgrounds/Bg1.png")
-
     image_editable = ImageDraw.Draw(background_image)
-    fontBig = ImageFont.truetype(
-        ".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 40)
-    fontMedium = ImageFont.truetype(
-        ".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 30)
-    fontSmall = ImageFont.truetype(
-        ".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 25)
-
-    # date
-    image_editable.text((620, 115), str(today),
-                        ("#FFFFFF"), font=fontMedium)
-
-    ranking = 8*counter - 8
+    fontBig = ImageFont.truetype(".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 40)
+    fontMedium = ImageFont.truetype(".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 30)
+    fontSmall = ImageFont.truetype(".\Assets\Fonts\Archivo_Black\ArchivoBlack-Regular.ttf", 25)
+    image_editable.text((620, 115), str(today),("#FFFFFF"), font=fontMedium)
+    ranking = 8*(counter - 1)
     for index in range(len(leaderboard_data)):
         item = leaderboard_data[index]
         ranking += 1
         offset = index+1
         row_y = -100 + (offset) * 100
-
-        # ranking
         if ranking > 9:
-            image_editable.text((85, 300 + row_y), str(ranking),
-                                ("#FFFFFF"), font=fontBig)
+            image_editable.text((85, 300 + row_y), str(ranking),("#FFFFFF"), font=fontBig)
         else:
-            image_editable.text((100, 300 + row_y), str(ranking),
-                                ("#FFFFFF"), font=fontBig)
-
-        # playername
-        image_editable.text((220, 275 + row_y+offset*2), item.name,
-                            ("#FFFFFF"), font=fontBig)
-        # playerdeck
-        image_editable.text((220, 330 + row_y), item.deck,
-                            ("#FFFFFF"), font=fontSmall)
-        # playerscore
-        image_editable.text((930, 300 + row_y),
-                            str(item.score), ("#FFFFFF"), font=fontBig)
-
+            image_editable.text((100, 300 + row_y), str(ranking), ("#FFFFFF"), font=fontBig)
+        image_editable.text((220, 275 + row_y+offset*2), item.name,("#FFFFFF"), font=fontBig)
+        image_editable.text((220, 330 + row_y), item.deck, ("#FFFFFF"), font=fontSmall)
+        image_editable.text((930, 300 + row_y), str(item.score), ("#FFFFFF"), font=fontBig)
         background_image.save('./Output/'+leaderboardName)
-
-
+        
 print("Generating Leaderboard.........")
 exportLeaderboard()
